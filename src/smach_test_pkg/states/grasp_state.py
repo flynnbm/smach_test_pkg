a95f83397649e8682e6da8588786ec07711a4d2f
flynnbm <flynn.m.brian@gmail.com>
2025-05-23 00:38:36 -0400

    rename state, this is just an example of a state that will call a grasp, TODO: allow topic modification so it can be used more generally

--- src/smach_test_pkg/states/grasp_state.py
+++ src/smach_test_pkg/states/grasp_state.py
@@ -3,22 +3,22 @@ import rclpy
 from geometry_msgs.msg import Pose
 from robot_common_manip.srv import FakeGrasp
 
-class FakeGraspState(smach.State):
+class GraspState(smach.State):
     def __init__(self, node, userdata, timeout_sec: float = 5.0):
         super().__init__(outcomes=['success', 'failure'])
         self.node = node
         self.userdata = userdata
         self.timeout_sec = timeout_sec
-        self.client = node.create_client(FakeGrasp, 'fake_grasp_pose')
+        self.client = node.create_client(FakeGrasp, 'grasp_pose')
 
     def on_enter(self):
-        self.node.get_logger().info("[FakeGraspState] Entering state.")
+        self.node.get_logger().info("[GraspState] Entering state.")
 
     def execute(self, _):
-        self.node.get_logger().info("[FakeGraspState] Waiting for service...")
+        self.node.get_logger().info("[GraspState] Waiting for service...")
 
         if not self.client.wait_for_service(timeout_sec=self.timeout_sec):
-            self.node.get_logger().error("[FakeGraspState] Service not available.")
+            self.node.get_logger().error("[GraspState] Service not available.")
             return 'failure'
 
         request = FakeGrasp.Request()
@@ -28,11 +28,11 @@ class FakeGraspState(smach.State):
         if future.result() is not None:
             grasp_pose = future.result().grasp_pose
             self.userdata.grasp_poses.append(grasp_pose)
-            self.node.get_logger().info(f"[FakeGraspState] Got grasp pose and stored in userdata.")
+            self.node.get_logger().info(f"[GraspState] Got grasp pose and stored in userdata.")
             return 'success'
         else:
-            self.node.get_logger().error("[FakeGraspState] Failed to call service.")
+            self.node.get_logger().error("[GraspState] Failed to call service.")
             return 'failure'
         
     def on_exit(self):
-        self.node.get_logger().info("[FakeGraspState] Exiting state.")
+        self.node.get_logger().info("[GraspState] Exiting state.")
